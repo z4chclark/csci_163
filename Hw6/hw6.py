55a2912773341f25dd7a95da1dedc1413940dcc4
zacharius <zacharyaclark@gmail.com>
2023-08-12 12:40:43 -0700

    hw6

--- Hw6/hw6.py
+++ Hw6/hw6.py
@@ -1,13 +1,15 @@
 import math
 
 def problem1 (N):
-    if (N < 0): return 0
-        
     if (N == 0): return 1
         
-    if (N == 1): return 3
-        
-    return problem1(int(N/2)) * problem1(math.ceil(N/2))
+    if N % 2 == 0:
+        half = problem1(N/2)
+        return half * half
+    else:
+        half = problem1((N-1)/2)
+        return 3 * half * half
 
 
-print(problem1(5000))
+
+print(problem1(500000))
