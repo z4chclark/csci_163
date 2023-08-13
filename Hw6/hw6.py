43019b42f09246543e817d1fe41a7da9ab56f8ac
zacharius <zacharyaclark@gmail.com>
2023-08-12 17:27:09 -0700

    k

--- Hw6/hw6.py
+++ Hw6/hw6.py
@@ -12,4 +12,24 @@ def problem1 (N):
 
 
 
-print(problem1(500000))
+print(problem1(5))
+
+
+
+def problem2 (A):
+    
+    negatives = []
+    positives = []
+    zeroes = []
+
+    for num in A:
+        if num < 0: negatives.append(num)
+
+        elif num > 0:positives.append(num)
+
+        else: zeroes.append(num)
+
+    return negatives + zeroes + positives
+
+A = [1, -18, 0, -3, 5, -5, 45]
+print(problem2(A))
